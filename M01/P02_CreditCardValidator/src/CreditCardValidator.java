2de69b9fcd037fe95fb6e0847718e5641eeb8ee0
Grant Simpson <grsimpson18@gmail.com>
2025-06-11 18:50:41 -0400

    created stubs

---
+++ M01/P02_CreditCardValidator/src/CreditCardValidator.java
@@ -0,0 +1,38 @@
+public class CreditCardValidator {
+    /** Return true if the card number is valid */
+    public static boolean isValid(long number) {
+        return true;
+    }
+
+    /** Get the result from Step 2 */
+    public static int sumOfDoubleEvenPlace(long number) {
+        return 0;
+    }
+
+    /** Return this number if it is a single digit, otherwise,
+     * return the sum of the two digits */
+    public static int getDigit(int number) {
+        return 0;
+    }
+
+    /** Return sum of odd-place digits in number */
+    public static int sumOfOddPlace(long number) {
+        return 0;
+    }
+
+    /** Return true if the number d is a prefix for number */
+    public static boolean prefixMatched(long number, int d) {
+        return true;
+    }
+
+    /** Return the number of digits in d */
+    public static int getSize(long d) {
+        return 0;
+    }
+
+    /** Return the first k number of digits from number. If the
+     * number of digits in number is less than k, return number. */
+    public static long getPrefix(long number, int k) {
+        return 0;
+    }
+}
