dc45c10dcc53d9835191dd545651a2289d1a4d9e
Grant Simpson <grsimpson18@gmail.com>
2025-06-13 17:44:00 -0400

    Finished

--- M01/P03_IdenticalArrays/src/Main.java
+++ M01/P03_IdenticalArrays/src/Main.java
@@ -1,10 +1,42 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main
 {
 
+    // The main program features two simple drivers: Interactive and Non-interactive.
+    // Interactive - User can input values into the two arrays. Enabled (uncommented out) by default.
+    // Non-interactive - A fixed set of values will be used to fill the arrays. Disabled (commented out) by default.
+    // To switch drivers, comment out the Interactive Driver section and uncomment the Non-interactive Driver section.
     public static void main(String[] args) {
-// Uncomment this code to use this as a driver instead of inputting the elements manually
+
+        /*************** INTERACTIVE DRIVER ***************/
+
+        System.out.println("Identical Arrays - v1.0.0");
+        Scanner input = new Scanner(System.in);
+        int [][] arr1 = new int[3][3];
+        int [][] arr2 = new int[3][3];
+        char doContinue = 'y';
+
+        while (doContinue == 'y') {
+            System.out.print("Enter the first 3 x 3 array: ");
+            get2dArray(arr1, input);
+            System.out.print("Enter the second 3 x 3 array: ");
+            get2dArray(arr2, input);
+
+            String equalStat = equals(arr1, arr2) ? "is equal"  : "is not equal";
+            System.out.printf("array 1 %s to array 2 %n", equalStat);
+
+            System.out.print("Do you want to continue (y/n): ");
+            doContinue = Character.toLowerCase(input.next().charAt(0));
+        }
+
+
+
+        /*************** NON-INTERACTIVE DRIVER ***************/
+
+//        System.out.println("Identical Arrays - v1.0.0");
 //        int[][] a =   {{1, 2, 1},
 //                {2, 1, 2},
 //                {1, 2, 1}};
@@ -15,13 +47,31 @@ public class Main
 //                {2, 1, 2},
 //                {1, 2, 1}};
 //
-//
 //        System.out.printf("a = b == %b%n", equals(a, b));
 //        System.out.printf("a = c == %b%n", equals(a, c));
 //        System.out.printf("b = c == %b%n", equals(b, c));
 
+
+
+    }
+    // Method to get a 3 x 3 2d array from user.
+    // The user can either enter a list of space separated characters or newline separated.
+    // If there is excess input (greater than 9 elements), the input is left in the buffer. This means that technically,
+    // although not recommended, one could enter both arrays in one line.
+    public static void get2dArray(int[][] arr, Scanner input) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (input.hasNextInt()) {
+                    arr[i][j] = input.nextInt();
+                }
+            }
+        }
     }
 
+    // Method to determine whether two 2d arrays are equal.
+    // Two arrays, a[][] and b[][] are equal if a[i][j] == b[i][j] for every i and j.
+    // The length is also checked because if two arrays differ in length they will never be equal,
+    // so it is not necessary to continue checking for equality after such a discovery.
     public static boolean equals(int[][] m1, int[][] m2) {
         if (m1.length != m2.length)
             return false;
