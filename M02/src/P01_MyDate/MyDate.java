750aefe76794f3eed453cc2a5c5550df691f3c5b
Grant Simpson <grsimpson18@gmail.com>
2025-06-17 15:14:49 -0400

    Created class diagram and class skeleton

---
+++ M02/src/P01_MyDate/MyDate.java
@@ -0,0 +1,20 @@
+package P01_MyDate;
+
+public class MyDate {
+private int year;
+private int month;
+private int day;
+MyDate() {}
+MyDate(long elpasedTime) {}
+MyDate(int year, int month, int day) {}
+int getYear() {
+    return year;
+}
+int getMonth() {
+    return month;
+}
+int getDay() {
+    return day;
+}
+void setDate(long elapsedTime) {}
+}
