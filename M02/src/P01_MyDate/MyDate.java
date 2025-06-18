3a7751ef84d3f9d7f0eef12e46d515c67b92efc0
Grant Simpson <grsimpson18@gmail.com>
2025-06-18 18:25:15 -0400

    a commit

--- M02/src/P01_MyDate/MyDate.java
+++ M02/src/P01_MyDate/MyDate.java
@@ -1,20 +1,38 @@
 package P01_MyDate;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class MyDate {
-private int year;
-private int month;
-private int day;
-MyDate() {}
-MyDate(long elpasedTime) {}
-MyDate(int year, int month, int day) {}
-int getYear() {
-    return year;
-}
-int getMonth() {
-    return month;
-}
-int getDay() {
-    return day;
-}
-void setDate(long elapsedTime) {}
+    private int year;
+    private int month;
+    private int day;
+
+    MyDate() {
+        year = GregorianCalendar.YEAR;
+        month = GregorianCalendar.MONTH;
+        day = GregorianCalendar.DAY_OF_MONTH;
+    }
+
+    MyDate(long elapsedTime) {
+        Calendar cal= new GregorianCalendar();
+        cal.setTimeInMillis(elapsedTime);
+    }
+
+    MyDate(int year, int month, int day) {
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDate(long elapsedTime) {
+    }
 }
