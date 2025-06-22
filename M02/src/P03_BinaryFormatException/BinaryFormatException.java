4ba348a18640f8a8f7c5841e48a3f82feee96d6f
Grant Simpson <grsimpson18@gmail.com>
2025-06-22 01:30:06 -0400

    Finished BinaryFormatException

---
+++ M02/src/P03_BinaryFormatException/BinaryFormatException.java
@@ -0,0 +1,8 @@
+package P03_BinaryFormatException;
+
+public class BinaryFormatException extends NumberFormatException {
+  public BinaryFormatException(String message) {
+    super(message);
+  }
+
+}
--- M02/src/P03_BinaryFormatException/Main.java
+++ M02/src/P03_BinaryFormatException/Main.java
@@ -1,14 +1,36 @@
 package P03_BinaryFormatException;
-import java.util.BinaryFormatException
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        String binaryNumber = "";
+        int decimalNumber = 0;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter Binary Number (enter -1 to quit): ");
+        binaryNumber = input.nextLine();
+        while (!binaryNumber.equals("-1")) {
+
+            try {
+                decimalNumber = bin2dec(binaryNumber);
+                System.out.println("Decimal Number: " + decimalNumber);
+            } catch (BinaryFormatException bfe) {
+                System.out.println("Error: " + bfe.getMessage());
+            }
+            System.out.println("Enter Binary Number (enter -1 to quit): ");
+            binaryNumber = input.nextLine();
+        }
+
     }
     public static int bin2dec(String bin) {
-       if (!isValidBinary(bin) {
-           throw new BinaryFormatException()
+       if (!isValidBinary(bin)) {
+           throw new BinaryFormatException("Invalid binary number");
         }
-        long num = Long.parseLong(bin, 2);
+       BigInteger dec = new BigInteger(bin, 2);
+       return dec.intValue();
 
     }
     public static boolean isValidBinary(String num) {
