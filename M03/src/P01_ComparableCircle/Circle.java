8093ebfbc77bdf2c0d706a1898d538d6c4f1aeb3
Grant Simpson <grsimpson18@gmail.com>
2025-06-25 20:18:32 -0400

    Refactor comments

--- M03/src/P01_ComparableCircle/Circle.java
+++ M03/src/P01_ComparableCircle/Circle.java
@@ -21,9 +21,10 @@ public class Circle extends GeometricObject
         this.radius = radius;
     }
 
-    @Override // Implement compareTo
+    // Implement compareTo
     // Returns a negative integer, zero, or a positive integer if
     // the object is less than, equal to, or greater than otherCircle, respectively.
+    @Override
     public int compareTo(Circle otherCircle) {
         return Double.compare(this.radius, otherCircle.radius);
     }
@@ -31,24 +32,21 @@ public class Circle extends GeometricObject
     // Override equals method
     // Returns true if the two circles have the same radius or if "this" and "o"
     // reference the same object, otherwise returns false.
-    @Override // Implement equals
+    @Override
     public boolean equals(Object o) {
         if (o == null) throw new NullPointerException();
-        // If object is compared to itself, return true
         if (this == o) return true;
-        // If object is null or the two object's classes differ return false
         if (getClass() != o.getClass()) return false;
         Circle circle = (Circle) o;
-        // If compare indicates equality (returning 0), return true
         return Double.compare(this.radius, circle.radius) == 0;
     }
 
-    /** Return radius */
+    // Return radius
     public double getRadius() {
         return radius;
     }
 
-    /** Set a new radius */
+    // Set a new radius
     public void setRadius(double radius) {
         this.radius = radius;
     }
@@ -58,17 +56,17 @@ public class Circle extends GeometricObject
         return radius * radius * Math.PI;
     }
 
-    /** Return diameter */
+    // Return diameter
     public double getDiameter() {
         return 2 * radius;
     }
 
-    @Override /** Return perimeter */
+    @Override // Return perimeter
     public double getPerimeter() {
         return 2 * radius * Math.PI;
     }
 
-    /* Print the circle info */
+    // Print the circle info
     public void printCircle() {
         System.out.println("The circle is created " + getDateCreated() +
                 " and the radius is " + radius);
